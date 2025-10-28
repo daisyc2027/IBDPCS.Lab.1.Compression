8afbe1e51184019c443f20bbc31dd4ba8e0de9a5
daisyc2027 <daisyc2027@student.cis.edu.hk>
2025-10-28 14:51:28 +0800

    done

--- src/main/java/org/example/RLE.java
+++ src/main/java/org/example/RLE.java
@@ -26,11 +26,50 @@ public class RLE {
     }
 
     /** TODO 1: Given a String (a genome sequence of COVID-19) implement the RLE algorithm that will use RLE to compress a String. Returns the compressed String. */
-    public static String compress(String uncompressed) { return null; }
+    public static String compress(String uncompressed) {
+        if (uncompressed.isEmpty()) return "";
+        StringBuilder str = new StringBuilder();
+        int count = 1;
+        char currentChar = uncompressed.charAt(0);
+        for (int i = 1; i < uncompressed.length(); i++) {
+            if (uncompressed.charAt(i) == currentChar) {
+                count++;
+            } else {
+                str.append(count).append(currentChar);
+                currentChar = uncompressed.charAt(i);
+                count = 1;
+            }
+        }
+        str.append(count).append(currentChar);
+
+        return str.toString();
+    }
 
     /** TODO 2: Given a String (a genome sequence of COVID-19) implement the RLE algorithm that will use RLE to decompress a String. Returns the uncompressed String. */
+
     public static String decompress(String compressed) {
-        return null;
+        StringBuilder str = new StringBuilder();
+        int i = 0;
+
+        while (i < compressed.length()) {
+            StringBuilder countStr = new StringBuilder();
+            while (i < compressed.length() && Character.isDigit(compressed.charAt(i))) {
+                countStr.append(compressed.charAt(i));
+                i++;
+            }
+
+            if (i < compressed.length()) {
+                char character = compressed.charAt(i);
+                int count = Integer.parseInt(countStr.toString());
+
+                for (int j = 0; j < count; j++) {
+                    str.append(character);
+                }
+                i++;
+            }
+        }
+
+        return str.toString();
     }
 
 
